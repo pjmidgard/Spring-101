1a18e2d427ae09b9c15c32899bb3393302281880
Java Software <pj.midgard@yahoo.com>
2022-05-28 13:23:06 +0100

    Update Spring-101.py

--- Spring-101.py
+++ Spring-101.py
@@ -958,11 +958,11 @@ class compression:
                                             Equal_info_between_of_the_cirlce_of_the_file_17=Equal_info_between_of_the_cirlce_of_the_file
 
 
-                                    if E==1 and Circle_times2>=255:
+                                    if E==1 and Circle_times2>=1000:
                                             compress_or_not_compress1=3
 
 
-                                    elif E==0 and Circle_times2==255:
+                                    elif E==0 and Circle_times2==1000:
                                             compress_or_not_compress1=3
                                             Equal_info_between_of_the_cirlce_of_the_file_17="9"+Equal_info_between_of_the_cirlce_of_the_file_17
 
