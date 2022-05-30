eb32d6d36cb6c2d052232e4cbaab9ace48741402
Java Software <pj.midgard@yahoo.com>
2022-05-30 18:32:36 +0100

    Update Spring-101.py

--- Spring-101.py
+++ Spring-101.py
@@ -903,8 +903,8 @@ class compression:
                                                 Block_101=1
                                                 
 
-                                            Block_file=Block_file+1024
-                                            Block_file2=Block_file2+1024
+                                            Block_file=Block_file+100
+                                            Block_file2=Block_file2+100
                                             Block_count=Block_count+1
 
                                            
@@ -1035,7 +1035,7 @@ class compression:
                                             Equal_info_between_of_the_cirlce_of_the_file_17=Equal_info_between_of_the_cirlce_of_the_file
 
 
-                                    if Circle_times2==(2**16)-1:
+                                    if Circle_times2==(2**8)-1:
                                             compress_or_not_compress1=3 
                   
 
